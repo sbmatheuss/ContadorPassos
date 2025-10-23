172bb571fbadd4cad46f3f9a7ce9a664d5151974
Matheus Braga <sbmatheus20@gmail.com>
2025-10-22 23:30:16 -0300

    estruturando projeto e iniciando o script

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+const frm = document.querySelector("form")
+const ini = document.getElementById("inicio")
+const fim = document.getElementById("fim")
+const res = document.getElementById("resultado")
+
+document.getElementById("contar").addEventListener("click", (e) => {
+    e.preventDefault()
+
+    const start = Number(inicio.value)
+    const end = Number(fim.value)
+    
+
+  
+})
